fb20e1ce6e4d200d2b8b44aa92b616b118a1f5a8
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-02-08 20:50:28 -0600

    Split out the cases

--- Tic_Tac_Toe/board.cpp
+++ Tic_Tac_Toe/board.cpp
@@ -1,1 +1,24 @@
 #include <iostream>
+#include <string>
+#include <array>
+#include "board.hpp"
+using namespace std;
+
+    Board::Board()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            board[i] = ' ';
+        }
+    }
+
+    void Board::make_move(int cell, char mark)
+    {
+        board[cell - 1] = mark;
+    }
+
+    string Board::get_mark(int cell)
+    {
+        return board[cell - 1];
+    }
+};
